c73a988e0410ae32038798ea1262877055f0a79c
WizzOFF <maher_off@proton.me>
2025-04-08 18:06:56 +0200

    manage_access_and_encapsulation.cpp: creating class

---
+++ manage_access_and_encapsulation.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+class Personnage
+{
+    // Méthodes
+    void recevoirDegats(int nbDegats)
+    {
+ 
+    }
+ 
+    void attaquer(Personnage &cible)
+    {
+ 
+    }
+ 
+    void boirePotionDeVie(int quantitePotion)
+    {
+ 
+    }
+ 
+    void changerArme(string nomNouvelleArme, int degatsNouvelleArme)
+    {
+ 
+    }
+ 
+    bool estVivant()
+    {
+ 
+    }
+ 
+    // Attributs
+    int m_vie;
+    int m_mana;
+    string m_nomArme;
+    int m_degatsArme;
+};
