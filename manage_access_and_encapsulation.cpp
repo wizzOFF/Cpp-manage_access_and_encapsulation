61775bc06de6450fd061607e0f8b5b3d5d01c4f9
WizzOFF <maher_off@proton.me>
2025-04-08 18:23:49 +0200

    manage_access_and_encapsulation.cpp: use of the object

--- manage_access_and_encapsulation.cpp
+++ manage_access_and_encapsulation.cpp
@@ -31,10 +31,26 @@ class Personnage
     {
  
     }
- 
+
     // Attributs
     int m_vie;
     int m_mana;
     string m_nomArme;
     int m_degatsArme;
 };
+
+int main()
+{
+    Personnage david, goliath;
+    //Création de 2 objets de type Personnage : david et goliath
+ 
+    goliath.attaquer(david); //goliath attaque david
+    david.boirePotionDeVie(20); //david récupère 20 de vie en buvant une potion
+    goliath.attaquer(david); //goliath attaque david
+    david.attaquer(goliath); //david contre-attaque... c'est assez clair non ?
+    
+    goliath.changerArme("Double hache tranchante veneneuse de la mort", 40);
+    goliath.attaquer(david);
+     
+    return 0;
+}
