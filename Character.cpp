3d7b8b9da01bfb84f863c02539ca6b2e27fe500f
WizzOFF <maher_off@proton.me>
2025-04-09 16:00:37 +0200

    Character.cpp: create a constructor / use alternative the initialization list

--- Character.cpp
+++ Character.cpp
@@ -2,11 +2,17 @@
 #include "Character.hpp"
 
 using namespace std;
-
+/*
 Personnage::Personnage()
 {
     m_vie = 100;
     m_mana = 100;
     m_nomArme = "Epee rouillee";
     m_degatsArme = 10;
+}
+*/
+
+Personnage::Personnage() : m_vie(100), m_mana(100), m_nomArme("Epee rouillee"), m_degatsArme(10)
+{
+    //Rien à mettre dans le corps du constructeur, tout a déjà été fait !
 }
