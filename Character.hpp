44f04c8bd06d6851e48dbf06597b85f64bc9040e
WizzOFF <maher_off@proton.me>
2025-04-09 14:51:58 +0200

    add main.cpp Character.hpp Character.cpp: initialize an object

--- Character.hpp
+++ Character.hpp
@@ -2,24 +2,4 @@
 #ifndef DEF_PERSONNAGE
 #define DEF_PERSONNAGE
 
-#include <string>
-
-class Personnage
-{
-    public:
-
-    void recevoirDegats(int nbDegats);
-    void attaquer(Personnage &cible);
-    void boirePotionDeVie(int quantitePotion);
-    void changerArme(std::string nomNouvelleArme, int degatsNouvelleArme);
-    bool estVivant();
-
-    private:
-
-    int m_vie;
-    int m_mana;
-    std::string m_nomArme; //Pas de using namespace std, il faut donc mettre std:: devant string
-    int m_degatsArme;
-};
-
 #endif
--- main.cpp
+++ main.cpp
@@ -6,15 +6,10 @@ using namespace std;
 
 int main()
 {
+    // Initialisez les attributs avec un constructeur
+    /*
     Personnage david, goliath;
-    //Création de 2 objets de type Personnage : david et goliath
-
-    goliath.attaquer(david); //goliath attaque david
-    david.boirePotionDeVie(20); //david récupère 20 de vie en buvant une potion
-    goliath.attaquer(david); //goliath attaque david
-    david.attaquer(goliath); //david contre-attaque... c'est assez clair non ? 
-    goliath.changerArme("Double hache tranchante veneneuse de la mort", 40);
-    goliath.attaquer(david);
+    */
 
     return 0;
 }
