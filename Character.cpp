e0b79b357241101b1c6a35e89c1fa0d3ad8050ff
WizzOFF <maher_off@proton.me>
2025-04-11 00:32:32 +0200

    corrected version

--- Character.cpp
+++ Character.cpp
@@ -11,12 +11,12 @@ Personnage::Personnage()
     m_degatsArme = 10;
 }
 */
-/*
+
 Personnage::Personnage() : m_vie(100), m_mana(100), m_nomArme("Epee rouillee"), m_degatsArme(10)
 {
     //Rien à mettre dans le corps du constructeur, tout a déjà été fait !
 }
-*/
+
 
 Personnage::Personnage(string nomArme, int degatsArme) : m_vie(100), m_mana(100),m_nomArme(nomArme), m_degatsArme(degatsArme)
 {
